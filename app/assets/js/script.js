f4f98ac691809f200b092b85de126168cab681df
Lyubo Velikov <lyubo.velikoff@gmail.com>
2017-07-06 22:34:56 +0100

    added login form + firebase functionality to authenticate users.

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,57 +1,61 @@
-var twitter = {};
-
-twitter.lastScrollTop = 0;
-twitter.ajaxCallsCount = 0;
-twitter.loading = false;
-
-
-twitter.manageNav = function() {
-    if ($(window).scrollTop() > 20) {
-        $('.main-title, .body-content').addClass('shrink');
-    } else {
-        $('.main-title, .body-content').removeClass('shrink');
-    }
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAc5m3FY73rw9NPBG1T0bqie1qtMeDzYkE",
+    authDomain: "shopping-list-b65e3.firebaseapp.com",
+    databaseURL: "https://shopping-list-b65e3.firebaseio.com",
+    projectId: "shopping-list-b65e3",
+    storageBucket: "shopping-list-b65e3.appspot.com",
+    messagingSenderId: "849292011260"
 };
+firebase.initializeApp(config);
 
 
-twitter.loadPosts = function() {
-    var source   = $("#post-template").html();
-    var template = Handlebars.compile(source);
-    twitter.loading = true;
-    $.ajax({
-        type: "GET",
-        url: "/twitter/app/includes/classes/api.php",
-        dataType: "json",
-        contentType: "application/json; charset=utf-8",
-        success: function(data) {
-            $(".posts").append(template(data));    
-            twitter.loading = false;
-            twitter.ajaxCallsCount++;
-        } 
-        // TODO: return error
+// Get Elements
+var txtEmail = document.getElementById('email');
+var txtPassword = document.getElementById('password');
+var btnLogin = document.getElementById('login-button');
+var btnSignup = document.getElementById('sign-button');
+var btnLogout = document.getElementById('logout-button');
+
+
+// Login Listener
+btnLogin.addEventListener('click', function(e) {
+    var email = txtEmail.value;
+    var password = txtPassword.value;
+    var auth = firebase.auth();
+    
+    var promise = auth.signInWithEmailAndPassword(email, password);
+    promise.catch(function(e) {
+        console.log(e.message);
     });
-}
-
-$(document).ready(function() {
-    twitter.manageNav();
-    twitter.loadPosts();
 });
-$(window).scroll(function (event) {
-    twitter.manageNav();
 
-    var top = $(this).scrollTop();
 
-    // load posts only when: 
-    // detect that the scrolling is down
-    // + the scroll reached 80%
-    // + its not loading at the moment
-    // + the number of calls < 5
-    if (top > twitter.lastScrollTop && top >= ($(document).height() - $(window).height()) * 0.8 && !twitter.loading && twitter.ajaxCallsCount < 5){
-        // downscroll code
-        twitter.loadPosts();
+// Sign up Listener
+btnSignup.addEventListener('click', function(e) {
+    var email = txtEmail.value;
+    var password = txtPassword.value;
+    var auth = firebase.auth();
+    
+    var promise = auth.createUserWithEmailAndPassword(email, password);
+    promise.catch(function(e) {
+        console.log(e.message);
+    });
+});
+
+// Logout listener
+btnLogout.addEventListener('click', function(e) {
+    firebase.auth().signOut();
+});
+
+// Add a realtime Firebase Auth listener
+firebase.auth().onAuthStateChanged(function(user) {
+    if(user) {
+        console.log(user);
+        btnLogout.classList.remove('hide');
     } else {
-        // upscroll code
+        console.log('not logged in');
     }
 
-   twitter.lastScrollTop = top;
 });
+
